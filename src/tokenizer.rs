4ab207425c73fe4f86185defb30f51b631a51bb1
Mako <jlauve@rsmw.net>
2016-08-14 19:27:08 -0400

    WIP: Tests

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,10 +1,12 @@
 use std::str::CharIndices;
 
+#[derive(Debug)]
 pub struct TokErr {
     pub location: usize,
     pub reason: ErrReason,
 }
 
+#[derive(Debug)]
 pub enum ErrReason {
     UnrecognizedToken,
     InvalidStringLiteral,
@@ -14,6 +16,7 @@ fn error<T>(r: ErrReason, l: usize) -> Result<T, TokErr> {
     Err(TokErr { location: l, reason: r })
 }
 
+#[derive(Debug, Eq, PartialEq)]
 pub enum Tok<'input> {
     EndLn,
     EndBlk,
@@ -50,7 +53,7 @@ pub enum Tok<'input> {
     OpAdd,
     OpSub,
 
-    Bar,
+    Pipe,
     Hole,
     Knot,
 
@@ -152,7 +155,7 @@ impl<'input> Tokenizer<'input> {
                 },
 
                 '-' => match self.bump() {
-                    Some((i1, '-')) => {
+                    Some((_, '-')) => {
                         let i_n = self.take_until(|c| c == '\n').unwrap();
                         Some(Ok((i0, Tok::EndLn, i_n)))
                     },
@@ -170,14 +173,48 @@ impl<'input> Tokenizer<'input> {
                 },
 
                 '>' => match self.bump() {
-                    Some((i1, ' ')) => {
+                    Some((_, ' ')) => {
                         Some(self.string_literal(i0))
                     },
 
                     _ => Some(error(ErrReason::InvalidStringLiteral, i0)),
                 },
 
-                c if c.is_alphanumeric() => {
+                '?' => unimplemented!(),
+
+                '\'' => {
+                    self.bump();
+                    match self.snake_case(i0) {
+                        Ok((start, Tok::NmFunc(s), end)) => {
+                            Some(Ok((start, Tok::NmLabel(s), end)))
+                        },
+                        e => Some(e),
+                    }
+                },
+
+                '+' => Some(Ok((i0, Tok::OpAdd, i0 + 1))),
+                '*' => Some(Ok((i0, Tok::OpMul, i0 + 1))),
+                '/' => Some(Ok((i0, Tok::OpDiv, i0 + 1))),
+                '_' => Some(Ok((i0, Tok::Hole, i0 + 1))),
+                '|' => Some(Ok((i0, Tok::Pipe, i0 + 1))),
+                ',' => Some(Ok((i0, Tok::OpComma, i0 + 1))),
+
+                '(' => Some(Ok((i0, Tok::LParen, i0 + 1))),
+                ')' => Some(Ok((i0, Tok::RParen, i0 + 1))),
+
+                '[' => Some(Ok((i0, Tok::LSquare, i0 + 1))),
+                ']' => Some(Ok((i0, Tok::RSquare, i0 + 1))),
+
+                '{' => Some(Ok((i0, Tok::LCurly, i0 + 1))),
+                '}' => Some(Ok((i0, Tok::RCurly, i0 + 1))),
+
+                c if c.is_alphabetic() => if c.is_lowercase() {
+                    unimplemented!()
+                } else {
+                    unimplemented!()
+                },
+
+                c if c.is_digit(10) => {
                     unimplemented!()
                 },
 
@@ -187,6 +224,24 @@ impl<'input> Tokenizer<'input> {
     }
 
     fn string_literal(&mut self, start: usize) -> TokResult<Tok<'input>> {
+        let terminate = |c: char| { c == '\n' };
+        let end = self.take_until(terminate).unwrap();
+        let contents = &self.text[start .. end];
+        Ok((start, Tok::LitStr(contents), end))
+    }
+
+    fn snake_case(&mut self, start: usize) -> TokResult<Tok<'input>> {
+        let terminate = |c: char| { !(c == '_' || c.is_alphanumeric()) };
+        let end = self.take_until(terminate).unwrap();
+        let contents = &self.text[start .. end];
+        Ok((start, Tok::NmFunc(contents), end))
+    }
+
+    fn camel_case(&mut self, start: usize) -> TokResult<Tok<'input>> {
+        unimplemented!()
+    }
+
+    fn screaming_case(&mut self, start: usize) -> TokResult<Tok<'input>> {
         unimplemented!()
     }
 }
@@ -205,5 +260,24 @@ impl<'input> Iterator for Tokenizer<'input> {
             Some(Err(TokErr { location, reason })) =>
                 Some(Err(TokErr { location: location+h, reason: reason })),
         }
+    }
+}
+
+#[test]
+fn quick_test() {
+    let mut tokenizer = Tokenizer::new("== start\n(ok)#ok", 0);
+
+    let expected = &[
+        Tok::Knot,
+        Tok::NmFunc("start"),
+        Tok::EndLn,
+        Tok::LParen,
+        Tok::NmFunc("ok"),
+        Tok::RParen,
+        Tok::LitAtom("ok"),
+    ];
+
+    for (wanted, got) in expected.iter().zip(tokenizer) {
+        assert_eq!(wanted, &(got.expect("Oh no").1));
     }
 }
