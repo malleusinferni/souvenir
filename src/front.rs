250fb7cccc3d92da12389a71c9a9088a4cc45238
Mako <jlauve@rsmw.net>
2016-08-24 17:51:28 -0400

    Work on front end

--- src/front.rs
+++ src/front.rs
@@ -10,16 +10,25 @@ use ast::*;
 
 use eval::rem::Supervisor;
 
+const EXT: &'static str = "svr";
+
 pub struct ModuleLoader {
     search_dirs: Vec<PathBuf>,
     loaded_modules: HashMap<Modpath, Module>,
-    wanted_modules: HashSet<Modpath>,
+    wanted_modules: HashMap<Modpath, WantedBy>,
+}
+
+#[derive(Debug)]
+pub enum WantedBy {
+    User,
+    OtherModule(Modpath),
 }
 
 #[derive(Debug)]
 pub enum PathError {
     InvalidMainModuleName(OsString),
     AreYouSureThisIsARegularFile(OsString),
+    SearchedAllThesePlaces(Vec<PathBuf>, Modpath, WantedBy),
 }
 
 #[derive(Debug)]
@@ -27,6 +36,8 @@ pub enum CompileError {
     IoError(io::Error),
     PathError(PathError),
     ParseError(String),
+    MissingModules(Vec<Modpath>),
+    NoModulesLoaded,
     UnimplementedFeature(String),
 }
 
@@ -44,52 +55,14 @@ impl From<PathError> for CompileError {
     }
 }
 
-fn get_stem(p: &Path) -> Result<&str, PathError> {
-    let os_stem = try!(p.file_stem().ok_or({
-        PathError::AreYouSureThisIsARegularFile(p.as_os_str().to_owned())
-    }));
-    
-    // TODO: Check extension?
-
-    os_stem.to_str().and_then(|str_stem| {
-        use tokenizer::*;
-
-        let mut tokens = Tokenizer::new(str_stem, 0);
-        match tokens.next() {
-            Some(Ok((0, Tok::NmFunc(_), _))) => match tokens.next() {
-                None => Some(str_stem),
-                _ => None,
-            },
-            _ => None,
+// impl From<???> for CompileError { ... }
+macro_rules! try_parse {
+    ($x:expr) => {
+        match $x {
+            Ok(x) => x,
+            Err(e) => return Err(CompileError::ParseError(format!("{:?}", e))),
         }
-    }).ok_or(PathError::InvalidMainModuleName(os_stem.to_owned()))
-}
-
-#[test]
-fn test_get_stem() {
-    assert_eq!(get_stem(Path::new("/home/code/main.svr")).unwrap(), "main");
-    assert!(get_stem(Path::new("not a valid name.svr")).is_err());
-}
-
-pub fn load(p: &Path) -> CompileResult<ModuleLoader> {
-    let root_module = Modpath(vec![try!(get_stem(p)).to_string()]);
-
-    let mut file = try!(File::open(p));
-    let mut source = String::new();
-
-    try!(file.read_to_string(&mut source));
-
-    use tokenizer::Tokenizer;
-    use parser::parse_Module;
-
-    let module = match parse_Module(&source, Tokenizer::new(&source, 0)) {
-        Ok(_) => unimplemented!(),
-        Err(e) => return Err(CompileError::ParseError({
-            format!("{:?}", e)
-        }))
-    };
-
-    Err(CompileError::UnimplementedFeature(format!("everything")))
+    }
 }
 
 pub struct ModuleLoaderBuilder {
@@ -109,8 +82,9 @@ impl ModuleLoaderBuilder {
     }
 
     pub fn build(self) -> ModuleLoader {
-        let mut wanted_modules = HashSet::new();
-        wanted_modules.insert(Modpath(vec![self.first_module_name]));
+        let mut wanted_modules = HashMap::new();
+        let root = Modpath(vec![self.first_module_name]);
+        wanted_modules.insert(root, WantedBy::User);
 
         ModuleLoader {
             search_dirs: self.search_dirs,
@@ -129,14 +103,115 @@ impl ModuleLoader {
     }
 
     pub fn load_next(&mut self) -> CompileResult<()> {
-        unimplemented!()
+        use self::hash_queue::*;
+
+        let (modpath, wanted_by) = match self.wanted_modules.pop() {
+            Some(m) => m,
+            None => return Ok(()),
+        };
+
+        let path = try!(self.guess_filename(&modpath, wanted_by));
+
+        let mut file = try!(File::open(&path));
+        let mut source = String::new();
+        try!(file.read_to_string(&mut source));
+
+        let module = try_parse!({
+            use tokenizer::Tokenizer;
+            use parser::parse_Module;
+
+            parse_Module(&source, Tokenizer::new(&source, 0))
+        });
+
+        for wanted_module in module.wanted_modules() {
+            if self.loaded_modules.contains_key(&wanted_module) { continue; }
+
+            self.wanted_modules.entry(wanted_module).or_insert({
+                WantedBy::OtherModule(modpath.clone())
+            });
+        }
+
+        self.loaded_modules.insert(modpath, module);
+
+        Ok(())
+    }
+
+    fn guess_filename(&self, modpath: &Modpath, wanted_by: WantedBy) -> CompileResult<PathBuf> {
+        let &Modpath(ref segments) = modpath;
+        let mut paths_tried = vec![];
+
+        for mut buf in self.search_dirs.iter().cloned() {
+            for seg in segments.iter() {
+                buf.push(seg);
+            }
+
+            buf.set_extension(EXT);
+
+            let path = try!(buf.canonicalize());
+
+            if path.is_file() {
+                return Ok(path)
+            } else {
+                paths_tried.push(buf);
+            }
+        }
+
+        let modpath = modpath.clone();
+        Err(PathError::SearchedAllThesePlaces(paths_tried, modpath, wanted_by).into())
     }
 
     pub fn all_loaded(&self) -> bool {
         self.wanted_modules.is_empty()
     }
 
-    pub fn compile(self) -> ! {
+    pub fn compile<R: Rng>(self, rng: R) -> CompileResult<Supervisor<R>> {
+        if !self.wanted_modules.is_empty() {
+            return Err(CompileError::MissingModules({
+                self.wanted_modules.into_iter().map(|(m, _)| m).collect()
+            }));
+        }
+
+        if self.loaded_modules.is_empty() {
+            return Err(CompileError::NoModulesLoaded);
+        }
+
+        let mut tree_walker = TreeWalker::new(self.loaded_modules);
+
         unimplemented!()
+    }
+}
+
+impl Module {
+    fn wanted_modules(&self) -> HashSet<Modpath> {
+        unimplemented!()
+    }
+}
+
+struct TreeWalker {
+    _phony: (),
+}
+
+impl TreeWalker {
+    fn new(modules: HashMap<Modpath, Module>) -> Self {
+        let _ = modules;
+        TreeWalker { _phony: () }
+    }
+}
+
+mod hash_queue {
+    use std::hash::Hash;
+    use std::collections::HashMap;
+
+    pub trait HashQueue<K, T> {
+        fn pop(&mut self) -> Option<(K, T)>;
+    }
+
+    impl<K, T> HashQueue<K, T> for HashMap<K, T> where K: Hash + Eq + Clone {
+        fn pop(&mut self) -> Option<(K, T)> {
+            match self.keys().cloned().next() {
+                None => None,
+                Some(key) => self.remove(&key).map(|v| (key, v)),
+            }
+        }
     }
 }
