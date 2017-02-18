f2e3f6fbf2af57ea2dbf6d09cae31715a754216a
Mako <jlauve@rsmw.net>
2017-02-18 15:24:07 -0500

    Consolidate helper tools

--- src/bin/pathtest.rs
+++
@@ -1,7 +0,0 @@
-use std::path::PathBuf;
-
-fn main() {
-    let mut path = PathBuf::from("/usr/bin/binhex.pl");
-    path.set_extension("");
-    println!("{}", path.display());
-}
--- src/bin/pp.rs
+++ src/bin/pp.rs
@@ -1,19 +1,52 @@
 extern crate souvenir;
+extern crate clap;
 
-use std::io::Read;
-
-use souvenir::ast::Module;
+enum Cmd {
+    DumpAst,
+    DumpRem,
+}
 
 fn main() {
-    let mut buf = String::new();
+    use clap::{App, Arg};
 
-    match std::io::stdin().read_to_string(&mut buf) {
-        Ok(_) => (),
-        Err(_) => return,
+    let matches = App::new("Souvenir pretty-printer")
+        .arg(Arg::with_name("ast")
+             .long("ast")
+             .help("Print the AST instead of the compiled code"))
+        .arg(Arg::with_name("PATH")
+             .index(1)
+             .required(true)
+             .help("Path to Souvenir source file or directory"))
+        .get_matches();
+
+    let filename = matches.value_of("PATH").unwrap();
+
+    let cmd = match matches.occurrences_of("ast") {
+        0 => Cmd::DumpRem,
+        _ => Cmd::DumpAst,
+    };
+
+    if let Err(err) = pp(filename, cmd) {
+        println!("Error:");
+        println!("{}", err);
     }
+}
 
-    let ast = Module::parse(&buf)
-        .expect("Parse failed");
+use souvenir::ast::Program;
+use souvenir::driver::Try;
 
-    println!("{:#?}", ast);
+fn pp(path: &str, cmd: Cmd) -> Try<()> {
+    let program = Program::load_from_path(path.as_ref())?;
+
+    match cmd {
+        Cmd::DumpAst => {
+            println!("{:#?}", program);
+        },
+
+        Cmd::DumpRem => {
+            println!("{}", program.compile()?);
+        },
+    };
+
+    Ok(())
 }
