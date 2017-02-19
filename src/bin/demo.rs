ba372ae1d9f2cb97258fa5d34e01ca54ea69284f
Mako <jlauve@rsmw.net>
2017-02-19 18:43:20 -0500

    Quick example of a (CLI) host environment

---
+++ src/bin/demo.rs
@@ -0,0 +1,38 @@
+extern crate souvenir;
+extern crate clap;
+
+use souvenir::ast::Program;
+use souvenir::driver::Try;
+
+fn main() {
+    use clap::{App, Arg};
+
+    let matches = App::new("Souvenir demo interface")
+        .arg(Arg::with_name("PATH")
+             .index(1)
+             .required(true)
+             .help("Path to execute"))
+        .arg(Arg::with_name("SCENE")
+             .index(2)
+             .required(true)
+             .help("Scene to perform"))
+        .get_matches();
+
+    let filename = matches.value_of("PATH").unwrap();
+    let scene = matches.value_of("SCENE").unwrap();
+
+    run_demo(&filename, &scene)
+        .unwrap();
+}
+
+use std::path::Path;
+
+fn run_demo<P: AsRef<Path>>(path: P, scene: &str) -> Try<()> {
+    let program = Program::load_from_path(path.as_ref())?.compile()?;
+
+    let mut interpreter = program.init().unwrap();
+
+    loop {
+        interpreter.dispatch();
+    }
+}
